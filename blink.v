9656df5c558fb19e050006d1f706a9f3d1b6dca3
Wallace Benites <148720798+wallaceeBenites@users.noreply.github.com>
2025-07-22 00:48:18 -0300

    teste ok

--- blink.v
+++ blink.v
@@ -6,5 +6,29 @@ module Blink #(
     output reg [7:0] leds
 );
 
+localparam ONE_SECOND  = CLK_FREQ;
+localparam TWO_SECOND  = 2*CLK_FREQ;
+localparam HALF_SECOND = CLK_FREQ / 2;
+
+reg [31:0] counter;
+
+always @(posedge clk ) begin
+    if (!rst_n) begin
+        counter <= 32'h0;
+        leds     <= 8'b0;
+    end else begin
+        if ((counter >= HALF_SECOND - 1) && (leds == 8'h00)) begin
+            counter <= 32'h0;
+            leds <= ~leds;
+        end 
+        else if ((counter >= TWO_SECOND - 1) && (leds == 8'hFF)) begin
+            counter <= 32'h0;
+            leds <= ~leds;
+        end 
+        else begin
+            counter <= counter + 1;
+        end
+    end
+end
     
 endmodule
